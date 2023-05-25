0eafdbf5f25b9a52e5b8c37187cff238323a30a4
Thomas Camminady <0milieux_member@icloud.com>
2023-05-25 13:07:15 +0200

    Release commit.

---
+++ tests/test_footer.py
@@ -0,0 +1,32 @@
+import os
+
+import altair as alt
+
+from camminapy.plot import Footer
+
+
+def test_footer_returns_subtitle_dict():
+    path = os.getcwd()
+    chart = alt.Chart().properties(
+        title={"text": "ACTUAL PLOT TITLE", **Footer(path).subtitle()}
+    )
+    assert isinstance(chart, alt.Chart)
+
+
+def test_footer_can_create_chart():
+    path = os.getcwd()
+
+    chart = alt.Chart().properties(title=Footer(path).create())
+    assert isinstance(chart, alt.Chart)
+
+
+def test_footer_returns_subtitle_dict_no_path():
+    chart = alt.Chart().properties(
+        title={"text": "ACTUAL PLOT TITLE", **Footer().subtitle()}
+    )
+    assert isinstance(chart, alt.Chart)
+
+
+def test_footer_can_create_chart_no_path():
+    chart = alt.Chart().properties(title=Footer().create())
+    assert isinstance(chart, alt.Chart)
